749acaaf10dc0026adca6ca33670793df814ff70
Amy Gori <amydgori@gmail.com>
2021-08-16 11:45:41 -0400

    Initial commit with models

---
+++ albums/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(
+        "Artist", on_delete=models.CASCADE, related_name="albums"
+    )
+    release_date = models.DateField()
+
+    def __repr__(self):
+        return f"<Album title={self.title}>"
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __repr__(self):
+        return f"<Artist name={self.name}>"
+
+    def __str__(self):
+        return self.name
